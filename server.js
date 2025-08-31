afbdca7687b5fc2979d692c7aa651660f1db0583
asnaarmeenayaz-sketch <asnaarmeenayaz@gmail.com>
2025-08-31 16:15:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -387,6 +387,5 @@ app.patch("/update_vehicle_status", async (req, res) => {
   }
 });
 
-const PORT = 5000;
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
